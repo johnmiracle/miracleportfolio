728777e85c7ec8756fec1b861145ae12f3416008
miracle <anajemiracle@gmail.com>
2020-05-12 16:02:12 +0100

    modified

---
+++ app.js
@@ -0,0 +1,74 @@
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const bodyParser = require("body-parser");
+const flash = require("connect-flash");
+const session = require("express-session");
+
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+
+// .env setup
+require("dotenv").config();
+
+const app = express();
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "/public")));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+// Express session middleware
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: true,
+    secret: process.env.secret,
+    cookie: { maxAge: 180 * 60 * 1000 },
+  })
+);
+
+// Connect-flash middleware
+app.use(require("connect-flash")());
+app.use(function (req, res, next) {
+  res.locals.flashes = req.flash();
+  next();
+});
+
+// flash
+app.use(flash());
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+app.listen(() => console.log(`server running on post ${process.env.PORT}`));
+
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const router = express.Router();
+const nodemailer = require("nodemailer");
+const mailgun = require("nodemailer-mailgun-transport");
+const auth = require("../config/nodemailer");
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Home" });
+});
+
+/* GET contact page. */
+router.get("/contact", function (req, res, next) {
+  res.render("contact", { title: "Contact" });
+});
+
+/* POST contact page. */
+router.post("/contact", function (req, res, next) {
+  const quoteDetail = `
+    You have a Contact Us request.\n\n
+    Client Details:\n 
+    FIrst Name: ${req.body.name}\n
+    Last Name: ${req.body.surname}\n
+    Email: ${req.body.email}\n
+    Phone: ${req.body.phone}\n
+
+    Message:\n ${req.body.message}\n
+  `;
+
+  const nodemailerMailgun = nodemailer.createTransport(mailgun(auth));
+
+  // mail detail
+  nodemailerMailgun.sendMail(
+    {
+      from: "salesontws@gmail.com",
+      to: "anajemiracle@gmail.com", // An array if you have multiple recipients.
+      subject: "Contact Us Request",
+      "h:Reply-To": `${req.body.email}`,
+      //You can use "text:" to send plain-text content. It's oldschool!
+      text: quoteDetail,
+    },
+    (err, info) => {
+      if (err) {
+        console.log(`Error: ${err}`);
+        req.flash("Danger", "error sending request!!!");
+        return res.redirect("/contact");
+      } else {
+        req.flash("success", "Request sent!!!");
+        return res.redirect("/contact");
+      }
+    }
+  );
+});
+
+/* GET project page. */
+router.get("/projects", function (req, res, next) {
+  res.render("project", { title: "Projects" });
+});
+
+/* GET phdproject page. */
+router.get("/phd_projects", function (req, res, next) {
+  res.render("phdproject", { title: "Projects" });
+});
+
+/* GET resume page. */
+router.get("/resume", function (req, res, next) {
+  res.render("resume", { title: "My Resume" });
+});
+
+module.exports = router;
